21c53e59cde93a6d1e78daf2f0507a68021dfddc
mlimaicof <mlimaico@icloud.com>
2022-06-25 10:37:36 -0500

    Creacion de tablas Rol y usuario

---
+++ InventarioVacunas/src/main/java/com/example/demo/entity/Usuario.java
@@ -0,0 +1,148 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Usuario implements Serializable{
+
+	private static final long serialVersionUID = -8799827598108652317L;
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native", strategy="native")
+	private Long id;
+	
+	@Column
+	private String Nombre;
+	@Column 
+	private String Apellidos;
+	@Column (unique = true) 
+	private String email;
+	@Column (unique = true)
+	private String usuario;
+	@Column
+	private String password;
+	
+	@Transient
+	private String confirmPassword;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "usuario_rol",
+					joinColumns = @JoinColumn(name="Usuario_Id"),
+					inverseJoinColumns = @JoinColumn(name="Rol_Id"))
+	private Set<Rol> roles;
+
+	public Usuario(Long id) {
+		super();
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return Apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		Apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public Set<Rol> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Rol> roles) {
+		this.roles = roles;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", Nombre=" + Nombre + ", Apellidos=" + Apellidos + ", email=" + email
+				+ ", usuario=" + usuario + ", password=" + password + ", confirmPassword=" + confirmPassword
+				+ ", roles=" + roles + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Apellidos, Nombre, confirmPassword, email, id, password, roles, usuario);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(Apellidos, other.Apellidos) && Objects.equals(Nombre, other.Nombre)
+				&& Objects.equals(confirmPassword, other.confirmPassword) && Objects.equals(email, other.email)
+				&& Objects.equals(id, other.id) && Objects.equals(password, other.password)
+				&& Objects.equals(roles, other.roles) && Objects.equals(usuario, other.usuario);
+	}
+	
+	
+
+}
